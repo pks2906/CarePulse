959857ed1f34da0feb867d9aadea470428bb0371
pks2906 <pratik1971sinha@gmail.com>
2024-08-13 00:20:43 +0530

    added form schema

--- components/forms/Patientform.tsx
+++ components/forms/Patientform.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+"use client"
+
+import { z } from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
 
 export const Patientform = () => {
   return (
     <div>Patientform</div>
   )
 }
+
+export default Patientform
