1d050be3f9551c2dee77d0d11d25836a4490c03a
pks2906 <pratik1971sinha@gmail.com>
2024-08-14 09:42:53 +0530

    form updated

--- components/forms/Patientform.tsx
+++ components/forms/Patientform.tsx
@@ -17,7 +17,7 @@ export enum FormFieldType {
   SKELETON = 'skeleton',
 }
 
- 
+
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
